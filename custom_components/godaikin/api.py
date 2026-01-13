b554bfeddc4de7a2e1c8a2f4fcf9c4f5b9f79773
Ian Dela Cruz <ian.delacruz@edgeworks.com.sg>
2026-01-14 05:27:00 +0800

    Change BASE_URL for Philippine region

--- custom_components/godaikin/api.py
+++ custom_components/godaikin/api.py
@@ -10,7 +10,7 @@ import logging
 from .auth import AuthClient
 from .types import *
 
-BASE_URL = "https://c7zkf7l933.execute-api.ap-southeast-1.amazonaws.com/prod/"
+BASE_URL = "https://jm41kogy2b.execute-api.ap-southeast-1.amazonaws.com/prod/"
 
 _LOGGER = logging.getLogger(__name__)
 
